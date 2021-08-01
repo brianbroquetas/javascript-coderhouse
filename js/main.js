ec93436be105008df9eb1d73b3a7989ced4ac572
BrianBroquetas <brian.broquetas@hotmail.com>
2021-07-31 23:42:01 -0300

    primera entrega

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let ingreseActividad = prompt("Ingrese una actividad");
+
+let ingreseHorarioActividad = prompt("Ingrese el horario de la actividad");
+
+alert(`Usted realizara ${ingreseActividad} a las ${ingreseHorarioActividad} hs`);
